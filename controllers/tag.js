a8ae6db5074f042bc02bc73fe03b17968de9fcb0
regola-Tacens <regolas-tacens@outlook.fr>
2021-09-15 08:20:27 +0200

    ajout des tags

--- controllers/tag.js
+++ controllers/tag.js
@@ -46,10 +46,34 @@ const eraseTag = async (req, res) => {
 
 }
 
+const updatedTag = async (req, res)=>{
+    // console.log('dans back controller : ', req.body)
+    // console.log('dans back controller tagId :', req.params.tagId)
+    const id = req.params.tagId
+    // console.log('id',   id)
+    const { message } = req.body
+    const newtag = { message, _id : req.params.tagId}
+    // console.log (newtag)
+    try {
+        const updatedTag = await tagModel.findByIdAndUpdate(id, newtag, { new:true})
+        res.json(updatedTag);
+    } catch (error) {
+        res.status(404).json({ message : err.message})
+    }
+}
+
+// const { id } = req.params
+// const { pictureName, description,img, themeLinked, creator } = req.body
+// if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+// const updatedPicture = { pictureName, description, img,themeLinked, creator, _id : req.params.id}
+// await pictureModel.findByIdAndUpdate(id, updatedPicture, { new:true})
+
 
 
 module.exports = {
     createTag: createTag,
-    eraseTag: eraseTag
+    eraseTag: eraseTag,
+    updatedTag: updatedTag
 }
 
--- routes/tag.js
+++ routes/tag.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const controllerMethod = require ('../controllers/tag.js')
 const createTag= controllerMethod.createTag
 const eraseTag= controllerMethod.eraseTag
+const updatedTag = controllerMethod.updatedTag
 
 
 const controllerMethod2 = require('../middleware/auth.js')
@@ -11,6 +12,8 @@ const auth = controllerMethod2.auth
 
 router.post('/createTag', auth, createTag);
 router.delete('/eraseTag/:tagId', auth, eraseTag )
+router.post('/updateTag/:tagId', auth, updatedTag)
 
 
 module.exports = router
+
